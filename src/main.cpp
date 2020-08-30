98d2afe3f56a41c65410f0603e0b165a361b2401
Antonio Vanegas <hpsaturn@gmail.com>
2020-08-30 22:11:35 +0200

    fixed some parse issues and strcopy improvements

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 #endif
 
 //define your default values here, if there are different values in config.json, they are overwritten.
-char influx_server[40] = "influxdb.canair.io";
-char influx_port[6]  = "8080";
-char influx_db[32] = "canairio";
+char influx_server[40];
+char influx_port[6];
+char influx_db[32];
 char devicename[32];
 
 //flag for saving data
@@ -17,25 +17,25 @@ bool shouldSaveConfig = false;
 
 //callback notifying us of the need to save config
 void saveConfigCallback () {
-  Serial.println("Should save config");
+  Serial.println(">WM: Should save config");
   shouldSaveConfig = true;
 }
 
 void setupSpiffs(){
   //clean FS, for testing
-  // SPIFFS.format();
+//   SPIFFS.format();
 
   //read configuration from FS json
-  Serial.println("mounting FS...");
+  Serial.println(">WM: mounting FS..");
 
   if (SPIFFS.begin()) {
-      Serial.println("mounted file system");
+      Serial.println(">WM: mounted file system");
       if (SPIFFS.exists("/config.json")) {
           //file exists, reading and loading
-          Serial.println("reading config file");
+          Serial.println(">WM: reading config file");
           File configFile = SPIFFS.open("/config.json", "r");
           if (configFile) {
-              Serial.println("opened config file");
+              Serial.println(">WM: opened config file:");
               // Allocate a temporary JsonDocument
               // Don't forget to change the capacity to match your requirements.
               // Use arduinojson.org/v6/assistant to compute the capacity.
@@ -43,31 +43,25 @@ void setupSpiffs(){
               // Deserialize the JSON document
               DeserializationError error = deserializeJson(json, configFile);
               if (error)
-                  Serial.println(F("Failed to read file, using default configuration"));
+                  Serial.println(F("-WM: Failed to read config file, using default configuration"));
 
-              Serial.println("\nparsed json");
+              Serial.println(">WM: parsed json");
 
-              strcpy(influx_server, json["hostname"]);
-              strcpy(influx_port, json["port"]);
-              strcpy(influx_port, json["influxdb"]);
-              strcpy(devicename, json["devicename"]);
-
-              // if(json["ip"]) {
-              //   Serial.println("setting custom ip from config");
-              //   strcpy(static_ip, json["ip"]);
-              //   strcpy(static_gw, json["gateway"]);
-              //   strcpy(static_sn, json["subnet"]);
-              //   Serial.println(static_ip);
-              // } else {
-              //   Serial.println("no custom ip in config");
-              // }
+              strlcpy(influx_server, json["hostname"] | influx_server, sizeof(influx_server));
+              strlcpy(influx_port, json["port"] | influx_port, sizeof(influx_port));
+              strlcpy(influx_db, json["influxdb"] | influx_db, sizeof(influx_db));
+              strlcpy(devicename, json["devicename"] | "", sizeof(devicename));
+              
+              configFile.close();
 
           } else {
-              Serial.println("failed to load json config");
+              Serial.println("-WM: failed to open json config!");
           }
+      } else {
+          Serial.println("-WM: config file not found!");
       }
   } else {
-      Serial.println("failed to mount FS");
+      Serial.println("-WM: failed to mount FS!");
   }
   //end read
 }
@@ -93,7 +87,7 @@ void setup() {
   WiFiManagerParameter custom_influx_server("server", "influx server", influx_server, 40);
   WiFiManagerParameter custom_influx_port("port", "influx port", influx_port, 6);
   WiFiManagerParameter custom_influx_db("influxdb", "database", influx_db, 32);
-  WiFiManagerParameter custom_devicename("devicename", "device name", "", 32);
+  WiFiManagerParameter custom_devicename("devicename", "device name", devicename, 32);
 
   //add all your parameters here
   wm.addParameter(&custom_devicename);
@@ -101,15 +95,8 @@ void setup() {
   wm.addParameter(&custom_influx_db);
   wm.addParameter(&custom_influx_port);
 
-  // set static ip
-  // IPAddress _ip,_gw,_sn;
-  // _ip.fromString(static_ip);
-  // _gw.fromString(static_gw);
-  // _sn.fromString(static_sn);
-  // wm.setSTAStaticIPConfig(_ip, _gw, _sn);
-
   //reset settings - wipe credentials for testing
-  wm.resetSettings();
+//   wm.resetSettings();
 
   //automatically connect using saved credentials if they exist
   //If connection fails it starts an access point with the specified name
@@ -128,7 +115,7 @@ void setup() {
   // wm.startConfigPortal("AutoConnectAP","password");
 
   //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
+  Serial.println(">WM: connected! :)");
 
   //read updated parameters
   strcpy(influx_server, custom_influx_server.getValue());
@@ -138,43 +125,35 @@ void setup() {
 
   //save the custom parameters to FS
   if (shouldSaveConfig) {
-    Serial.println("saving config");
-    StaticJsonDocument<256> json;
+    Serial.println(">WM: saving config");
+    StaticJsonDocument<512> json;
     json["devicename"] = devicename;
     json["hostname"] = influx_server;
     json["port"]   = influx_port;
     json["influxdb"]   = influx_db;
 
-    // json["ip"]          = WiFi.localIP().toString();
-    // json["gateway"]     = WiFi.gatewayIP().toString();
-    // json["subnet"]      = WiFi.subnetMask().toString();
-
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile) {
-      Serial.println("failed to open config file for writing");
+      Serial.println(">WM: failed to open config file for writing");
     }
     // Serialize JSON to file
     if (serializeJson(json, configFile) == 0) {
-        Serial.println(F("Failed to write to file"));
+        Serial.println(">WM: Failed to write to file");
     }
     configFile.close();
-    //end save
     shouldSaveConfig = false;
   }
 
-  Serial.println("local ip");
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.gatewayIP());
-  Serial.println(WiFi.subnetMask());
-
   Serial.print(">WM: IP:\t");   Serial.println(WiFi.localIP());
   Serial.print(">WM: SSID:\t"); Serial.println(wm.getWiFiSSID());
+  Serial.print(">WM: GW:\t"); Serial.println(WiFi.gatewayIP());
   Serial.print(">WM: MAC:\t");  Serial.println(WiFi.macAddress());
   Serial.print(">WM: RSSI:\t"); Serial.println(WiFi.RSSI());
+  Serial.print(">WM: device:\t"); Serial.println(devicename);
+  Serial.print(">WM: server:\t"); Serial.println(influx_server);
+  Serial.print(">WM: port:\t"); Serial.println(influx_port);
+  Serial.print(">WM: database:\t"); Serial.println(influx_db);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-
 }
