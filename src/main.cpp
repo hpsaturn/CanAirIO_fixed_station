7fb415df395c6e82fa4bccab90e2d2a654895a3a
Antonio Vanegas <hpsaturn@gmail.com>
2020-09-15 23:19:44 +0200

    separated library via precompiler

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <WiFiConnect.hpp>  // Abstraction and handler of WifiManager
+
+#ifdef ESP32
 #include <ESP32Ping.h>      // Only for tests
-// #include <ESP8266Ping.h>
+#elif ESP8266
+#include <ESP8266Ping.h>
+#endif
+
+
 
 #define GPIO_LED_GREEN          22  // Led on TTGO board (black)
 
